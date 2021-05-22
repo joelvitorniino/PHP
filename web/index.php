58447730e22d1c4540cbf17ca5db5b1df4db1d76
Joel Vitor Niino Campos <73134353+joelvitorniino@users.noreply.github.com>
2021-05-22 18:09:27 -0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,3 +1,14 @@
-<?php
-echo "Hello, World"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script type="text/javascript">
+        window.location.href = 'https://projetoexpress.online'
+    </script>
+</body>
+</html>
